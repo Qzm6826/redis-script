607bafe08b995923c3b31730caf49407b6e23b6c
lwy <1139038165@qq.com>
2019-08-23 09:53:40 +0800

    修改：     Application/Config.example.php 	修改：     README.md

--- Application/Config.example.php
+++ Application/Config.example.php
@@ -11,11 +11,19 @@ class Config{
         'db' => 8
     );
 
+    /**
+     * HKeys : 哈希key数组
+     * @var array
+     */
     static $HKeys = array(
         'hall1',
         "hall2"
     );
 
+    /**
+     * Keys : 字符串key前缀，哈希key
+     * @var array
+     */
     static $Keys = array(
         'hall3',
         'str'
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 需要依赖与`redis-cli`  
-linux定时器`crontab`
+linux定时器`crontab`  
+配置文件 `Application/Config.php`，cp `Config.example.php` 即可
